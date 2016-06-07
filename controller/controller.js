a7aa0d383525f527b68f0e870a1c7c3947375ded
Kostas Gan <kostasgan@e-food.gr>
2016-06-07 23:10:24 +0300

    added check for servers results length

--- controller/controller.js
+++ controller/controller.js
@@ -5,6 +5,10 @@ function test(){
 
 	http.onreadystatechange = function(){
 		if(http.readyState == 4 && http.status == 200) {
+
+			if(http.responseText.length == 0){
+				alert('Δεν βρέθηκαν αποτελέσματα!')
+			}
 			console.log(http.responseText);
 		}
 	}
