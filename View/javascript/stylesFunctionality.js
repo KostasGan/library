86030b7f831f1d5e04d2fb26c8d2343f7bde917a
Kostas Gan <kostasgan@e-food.gr>
2016-06-08 21:52:17 +0300

    add function which get books titles from server and add them as option in Titles select

--- View/javascript/stylesFunctionality.js
+++ View/javascript/stylesFunctionality.js
@@ -1,4 +1,4 @@
-function onSelectionChange(){
+function onSearchOptionChange(){
 	var elem = document.getElementById("search-selection");
 	var userOption = elem.options[elem.selectedIndex].value;
 
@@ -10,4 +10,15 @@ function onSelectionChange(){
 		document.getElementById("formForKeyword").style.display="block";
 		document.getElementById("TitleSelection").style.display="none";
 	}
+}
+
+function addSelectOptions(obj){
+	var select = document.getElementById('selectbytitle');
+	var option =document.createElement('option');
+	option.text= obj.books[1].title;
+	select.add(option);
+}
+
+function BookPresentation(books) {
+	// body...
 }
