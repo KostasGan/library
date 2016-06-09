484aa6b815813cd48a4f1a42a765f2c39c5b34f4
Kostas Gan <kostasgan@e-food.gr>
2016-06-09 23:27:36 +0300

    functionality for getBookByTitle

---
+++ controller/getBookByTitles.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'db_connection.php';
+
+$db = new Database();
+$title = $_GET['title'];
+
+if(!isset($title) or empty($title)){
+	echo "Δεν έχετε επιλέξει τίτλο βιβλίου!";
+	exit;
+} ;
+
+$query = "SELECT * FROM books WHERE title='".$title."'";
+
+$conn = $db->db_connections();
+$results = $db->data_selection($conn,$query);
+$db->db_disconnection($conn);
+
+
+return $title;
+
+?>
