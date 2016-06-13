3aea6935f929544a8fe87c56658ceb035732d15f
KostasG <kostasgan@e-food.gr>
2016-06-13 18:37:20 +0300

    first try to print books on frontend

--- View/javascript/stylesFunctionality.js
+++ View/javascript/stylesFunctionality.js
@@ -26,5 +26,59 @@ function addSelectOptions(obj){
 }
 
 function BookPresentation(books) {
-	// body...
+	var text = "";
+	var books_div = document.getElementById('books');
+	books_div.style.display='block';
+
+	if (books_div.hasChildNodes()) {
+		if (books_div.childNodes.length == 1 && books.books.length == 1 ) {
+			books_div.removeChild(books_div.childNodes[0]);
+		}
+		else{
+			for(j=0; j<books_div.childNodes.length; j++){
+				books_div.removeChild(books_div.childNodes[j]);
+			}
+		}		
+	};
+
+
+
+	for (i=0; i<books.books.length; i++) {
+		var new_book = document.createElement('div');
+
+		var title = document.createElement('h1');
+		title.setAttribute('class','book_title');
+		text = document.createTextNode(books.books[i].title + " - " + books.books[i].publish_date);
+		title.appendChild(text);
+
+		var author = document.createElement('p');
+		author.setAttribute('class','book_author');
+		text = document.createTextNode("Συγγραφέας: " + books.books[i].author);
+		author.appendChild(text);
+
+		var price = document.createElement('p');
+		price.setAttribute('class','book_price');
+		text = document.createTextNode("Τιμή: " + books.books[i].price + " Euro");
+		price.appendChild(text);
+
+		var genre = document.createElement('p');
+		genre.setAttribute('class','book_genre');
+		text = document.createTextNode("Είδος Βιβλίου: " + books.books[i].genre);
+		genre.appendChild(text);
+
+		var description = document.createElement('p');
+		description.setAttribute('class','book_description');
+		text = document.createTextNode("Περιγραφή: "+books.books[i].description);
+		description.appendChild(text);
+
+		new_book.appendChild(title);
+		new_book.appendChild(author);
+		new_book.appendChild(price);
+		new_book.appendChild(genre);
+		new_book.appendChild(description);
+
+
+		books_div.appendChild(new_book);
+	};
+
 }
