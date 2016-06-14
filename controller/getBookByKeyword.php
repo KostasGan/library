f69688c9b8d5788972f72895a9a4eee16939997c
Kostas Gan <kostasgan@e-food.gr>
2016-06-14 21:07:39 +0300

    comments

--- controller/getBookByKeyword.php
+++ controller/getBookByKeyword.php
@@ -2,28 +2,28 @@
 
 require_once 'db_connection.php';
 
-$db = new Database();
-$keyword = $_POST["keyword"];
+$db = new Database(); // create a controller type Database for db_connection.php
+$keyword = $_POST["keyword"]; //get user's input from url as parameter
 
-
+//check if parameter has value or is empty and return a error message.
 if(!isset($keyword) or empty($keyword)){
 	echo "Δεν έχετε εισάγει λέξη αναζήτησης";
 	exit;
 } ;
 
+//sql query that return books which author,title,genre,description contains the user's input.
 $query = "SELECT * FROM books 
 		  WHERE  author LIKE '%".$keyword."%' 
 		  OR title LIKE '%".$keyword."%' 
 		  OR genre LIKE '%".$keyword."%' 
 		  OR description LIKE '%".$keyword."%'";
 
-$conn = $db->db_connections();
-$results = $db->data_selection($conn,$query);
-$db->db_disconnection($conn);
+$conn = $db->db_connections(); //call db_connections() for connection to database and save it in a variable
+$results = $db->data_selection($conn,$query); //call data_selection() that get as parameter a connection and a query
+												//and execute query for the required data.
+$db->db_disconnection($conn); //call db_disconnections() to close connection with database.
 
 
-return $keyword;
-
-
+return $keyword; //return the results from the db to client controller for use.
 
 ?>
--- controller/getBookByTitles.php
+++ controller/getBookByTitles.php
@@ -2,22 +2,24 @@
 
 require_once 'db_connection.php';
 
-$db = new Database();
-$title = $_POST["title"];
+$db = new Database(); //create a controller type Database for db_connection.php
+$title = $_POST["title"]; //get user's input from url as parameter
 
-
+//check if parameter has value or is empty and return a error message.
 if(!isset($title) or empty($title)){
 	echo "Δεν έχετε επιλέξει τίτλο βιβλίου!";
 	exit;
 } ;
 
+//sql query that return book which title is the same as user's selected
 $query = "SELECT * FROM books WHERE title='".$title."'";
 
-$conn = $db->db_connections();
-$results = $db->data_selection($conn,$query);
-$db->db_disconnection($conn);
+$conn = $db->db_connections(); //call db_connections() for connection to database and save it in a variable
+$results = $db->data_selection($conn,$query); //call data_selection() that get as parameter a connection and a query
+												//and execute query for the required data.
+$db->db_disconnection($conn); //call db_disconnections() to close connection with database.
 
 
-return $title;
+return $title; //return the results from the db to client controller for use.
 
 ?>
