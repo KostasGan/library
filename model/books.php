290b8ae848cddbabb92143f77ffff88c7dc6907b
Kostas Gan <kostasgan@e-food.gr>
2016-06-16 23:50:56 +0300

    rewrite model

--- model/books.php
+++ model/books.php
@@ -1,64 +1,24 @@
 <?php
-class Model{
+class Books{
 
-	private $id="";
-	private $author="";
-	private $title="";
-	private $genre="";
-	private $price=0;
-	private $publish_date="";
-	private $description="";
+	private $books;
 	
-	public function __contruct($id,$author,$title,$genre,$price,$publish_date,$description)
+	//return Books data to controller in JSON format.
+	public function getBooks()
 	{
-		$this->id=$id;
-		$this->author=$author;
-		$this->title=$title;
-		$this->genre=$genre;
-		$this->price=$price;
-		$this->publish_date=$publish_date;
-		$this->description=$decription;
+		return $this->JSONStringOutput($this->books);
 	}
 
-
-	public function getID()
+	//set books data from database
+	public function setBooks($books)
 	{
-		return $this->id;
+		$this->books=$books;
 	}
 
-	public function getAuthor()
-	{
-		return $this->author;
-	}
-	public function getTitle()
-	{
-		return $this->title;
-	}
-	public function getGenre()
-	{
-		return $this->genre;
-	}
-	public function getPrice()
-	{
-		return $this->price;
-	}
-	public function getPublish_Date()
-	{
-		return $this->publish_date;
-	}
-	public function getDescription()
-	{
-		return $this->description;
-	}
-
+	//convert data array from database to JSON and return it.
 	public function JSONStringOutput($results)
 	{
-		$emparray = array();
-    	while($row =$results->fetch_assoc())
-    	{
-        	$emparray[] = $row;
-    	}
-    	echo '{"books":'.json_encode($emparray, JSON_UNESCAPED_UNICODE).'}';
+    	echo '{"books":'.json_encode($results, JSON_UNESCAPED_UNICODE).'}';
 	}
 }
 
